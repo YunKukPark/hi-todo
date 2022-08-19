209d53a093092520a2a045a3f0dca5bf5424430b
YunKukPark <a01043340999@gmail.com>
2022-08-19 16:01:31 +0900

    Init: Project settings

---
+++ src/Router.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Login from 'Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
